4902d0c7b370680e6ae2e584072f68f86fb89e6d
Antoine Lorence <antoine.lorence@gmail.com>
2012-07-09 23:28:49 +0200

    Add missing class

---
+++ src/RenameEngine.py
@@ -0,0 +1,81 @@
+'''
+Created on 5 juil. 2012
+
+@author: Antoine Lorence
+'''
+
+import os
+import re
+
+
+DEFAULT_PATERN = "%artist% - %title%"
+
+class RenameEngine(object):
+
+    def __init__(self, inputFileOrFolder, connector, pattern=None):
+        '''
+        Constructor
+        '''
+        self.input = inputFileOrFolder
+        self.connector = connector
+        self.fileList = self.getFiles()
+        if None == pattern:
+            self.pattern = DEFAULT_PATERN
+        else:
+            self.pattern = pattern
+    
+    def getFiles(self, path=None):
+        fileList = []
+        if path == None :
+            return self.getFiles(self.input)
+        else:
+            for root, dirs, files in os.walk(path) :
+                for f in files:
+                    fileList.append(root + os.path.sep + f)
+                for d in dirs:
+                    fileList.extend(self.getFiles(root + os.path.sep + d))
+            return fileList
+        
+    def renameFiles(self):
+        renameList = []
+        idList = []
+        for _file in self.fileList:
+            try:
+                infos = self.getIdFromFile(_file)
+                if infos['id'] != "":
+                    idList.append(infos['id'])
+                    renameList.append(infos)
+            except:
+                pass
+        
+        tracksList = self.connector.getTracksFromIds(idList)
+        for infos in renameList:
+            trackObj = tracksList[infos['id']]
+            infos['obj'] = trackObj
+        
+        for rename in renameList:
+            
+            oldFile = rename['dir'] + os.path.sep + rename['file']
+            
+            suffix = getSuffix(rename['file'])
+            trackObj = rename['obj']
+            newFile = rename['dir'] + os.path.sep + trackObj.getArtists() + ' - ' + trackObj.title + '.' + suffix
+            
+            os.rename(oldFile, newFile)
+        
+            print oldFile + " renamed into " + newFile + "\n"
+        
+    
+    def getIdFromFile(self, filePath):
+        info = {}
+        separatorPos = filePath.rfind(os.path.sep)
+        d = filePath[0:separatorPos]
+        f = filePath[separatorPos + 1:]
+        idT = re.match(r"([0-9]+).*\.(wav|flac|mp3|aiff|aif)", f).group(1)
+        info['id'] = idT
+        info['dir'] = d
+        info['file'] = f
+        return info
+    
+def getSuffix(fileName):
+    return fileName[fileName.rfind(".")+1:]
