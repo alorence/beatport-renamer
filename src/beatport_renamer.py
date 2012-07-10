c8b6ffa54f1745478ef9712d72bb148253fa5846
Antoine Lorence <antoine.lorence@gmail.com>
2012-07-10 13:07:35 +0200

    Set version to 0.1.1

--- src/beatport_renamer.py
+++ src/beatport_renamer.py
@@ -8,20 +8,23 @@ import argparse
 from RenameEngine import RenameEngine
 from connectors.Beatport import Beatport
 
-VERSION = 0.1
+VERSION = "0.1.1"
 
 def setupCommandLine():
     # Help on arparse usage module : http://docs.python.org/library/argparse.html#module-argparse
     global parser
     
-    parser = argparse.ArgumentParser(description='This program help you to rename tracks purchased from http:://www.beatport.com', version=VERSION)
+    parser = argparse.ArgumentParser(description='This program helps you to rename tracks purchased from http:://www.beatport.com', version=VERSION)
 
     parser.add_argument('-i', '--input', action="store", dest="path", required=True,
                         help="Input file or folder to search in")
+    
+    parser.add_argument('-r', '-R', action="store_true", dest="recursive",
+                        help="IF set, files are searched in 'input' directory and its sub-directories")
     
 if __name__ == '__main__':
     setupCommandLine()
     arguments = parser.parse_args()
     bp = Beatport()
-    engine = RenameEngine(arguments.path, bp)
+    engine = RenameEngine(arguments.path, bp, arguments.recursive)
     engine.renameFiles()
