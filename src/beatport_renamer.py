d20577afe72239728a9cd8bc157e8a4a095a3d2c
Antoine Lorence <antoine.lorence@gmail.com>
2012-07-10 16:34:44 +0200

    Fix help messages

--- src/beatport_renamer.py
+++ src/beatport_renamer.py
@@ -17,10 +17,10 @@ def setupCommandLine():
     parser = argparse.ArgumentParser(description='This program helps you to rename tracks purchased from http:://www.beatport.com', version=VERSION)
 
     parser.add_argument('-i', '--input', action="store", dest="path", required=True,
-                        help="Input file or folder to search in")
+                        help="Input folder to search in")
     
     parser.add_argument('-r', '-R', action="store_true", dest="recursive",
-                        help="IF set, files are searched in 'input' directory and its sub-directories")
+                        help="Search for files recursively (in 'input' and its sub-directories)")
     
 if __name__ == '__main__':
     setupCommandLine()
